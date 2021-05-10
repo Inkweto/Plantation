15955f80633f6a8770dee186592db67c95c7391e
arczi1 <ksesese1@gmail.com>
2021-05-10 21:29:29 +0200

    User model added

---
+++ src/main/kotlin/ninja/plantation/api/model/User.kt
@@ -0,0 +1,26 @@
+package ninja.plantation.api.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "users")
+data class Users(
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long?, 
+
+    @Column(name = "login")
+    var login: String,
+
+    @Column(name = "password_hash")
+    var password_hash: String
+){
+	override fun toString(): String{
+        return "Users[id=${id}, login=${login}, password_hash=${password_hash}]"
+	}
